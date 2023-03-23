44418b15c486a521e40edaa4e1cd215baf0f9cb5
Ildar <akhmilg@gmail.com>
2023-03-23 18:04:56 +0500

    Added Task 049

---
+++ Task049/Program.cs
@@ -0,0 +1,55 @@
+﻿// Задача 48: Задайте двумерный массив.
+// Найдите элементы, у которых оба индекса четные, и замените
+// эти элементы на их квадраты.
+
+
+int[,] CreateMatrixRndInt(int raws, int columns, int min, int max)
+{
+    int[,] matrix = new int[raws, columns];
+
+    Random rnd = new Random();
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            matrix[i, j] = rnd.Next(min, max + 1);
+        }
+
+
+    }
+    return matrix;
+}
+
+
+void PrintMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        Console.Write("|");
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+             Console.Write($"{matrix[i,j], 6} ");
+        }
+        Console.WriteLine("|");
+    }
+}
+
+void ChangeMatrixSquareInt(int raws, int columns, int min, int max)
+{
+      
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            if(i%2==0&&j%2==0)
+            matrix[i, j] = (matrix[i, j]*matrix[i, j]);
+        }
+    }
+    return matrix;
+}
+
+int[,] array2D=CreateMatrixRndInt(3, 4, 0, 10);
+PrintMatrix(array2D);
+ChangeMatrixSquareInt(array2D);
+PrintMatrix(array2D);
